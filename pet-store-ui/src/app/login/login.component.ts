b986ced11491f2f6ef16a8eec2f9e7e053145d66
Alberto Di Martino <albertodmartino@gmail.com>
2018-07-06 14:05:39 -0700

    Login form

--- pet-store-ui/src/app/login/login.component.ts
+++ pet-store-ui/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm: FormGroup;
+  loginError: boolean = false;
+  
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.createLoginForm();
+  }
+
+  createLoginForm() {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  submitLoginForm() {
+    console.log("Form submitted!!");
   }
 
 }
